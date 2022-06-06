f4e5e380d73ee8580c87ae65d4903f459dca3975
clonomaer <clonomaer@gmail.com>
2022-06-06 14:57:48 +0200

    mod improvements on swap page animations

--- src/routes/swap.svelte
+++ src/routes/swap.svelte
@@ -13,6 +13,7 @@
   import ProgressBar from '$lib/ProgressBar.svelte'
   import SwapCard from '$lib/SwapCard.svelte'
   import _ from 'lodash'
+  import { fade } from 'svelte/transition'
 
   let isLoadingRaised = true
   $: isLoadingRaised = _.isUndefined($preSaleTotalCollateralRaised$) || _.isUndefined($preSaleGoal$)
@@ -20,60 +21,50 @@
   $: notFunding = _.isNull($preSaleTotalCollateralRaised$) || _.isNull($preSaleGoal$)
 </script>
 
-<div class="flex flex-col items-center">
-  <Fade
-    visible={$preSaleStatus$ === undefined}
-    className={{
-      container: `flex flex-col max-w-full ${$preSaleStatus$ === undefined && 'grow'}`,
-      wrapper: 'grow',
-    }}
-    mode="height">
-    <div class="md:grow md:h-full h-96 flex items-center justify-center">
+<div class="flex flex-col items-center relative z-0 min-h-[theme(spacing.96)] grow">
+  {#if $preSaleStatus$ === undefined}
+    <div transition:fade class="absolute z-0 inset-0 flex items-center justify-center">
       <LoadingSpinner />
     </div>
-  </Fade>
-  <Fade
-    visible={$preSaleStatus$ === PreSaleStatus.Pending}
-    className={{
-      container: `flex flex-col max-w-full ${$preSaleStatus$ === PreSaleStatus.Pending && 'grow'}`,
-      wrapper: 'grow',
-    }}
-    mode="height">
-    <div class="md:grow md:h-full h-96 flex items-center justify-center">
-      {$__$?.presale.stillPending}
+  {/if}
+  {#if $preSaleStatus$ === PreSaleStatus.Pending || $preSaleStatus$ === null}
+    <div transition:fade class="flex flex-col max-w-full grow">
+      <div class="grow flex items-center justify-center">
+        {$__$?.presale.stillPending}
+      </div>
     </div>
-  </Fade>
-  <Fade
-    className={{ container: 'max-w-full' }}
-    visible={!_.isUndefined($preSaleStatus$) && $preSaleStatus$ !== PreSaleStatus.Pending}
-    mode="height">
-    <div class="flex flex-col space-y-6">
-      <PageTitle />
-      <div
-        class="flex flex-col items-stretch md:flex-row-reverse justify-center md:space-x-reverse md:space-x-7 space-y-6 md:space-y-0">
-        <SwapCard />
-        <div class="flex flex-col gap-6 justify-between md:w-96">
-          <PreSaleAllocationTable />
-          <div class="flex flex-col justify-center grow">
-            <Card>
-              <ProgressBar
-                isLoading={isLoadingRaised}
-                disabled={notFunding}
-                percent={(($preSaleTotalCollateralRaised$ ?? 0) / ($preSaleGoal$ ?? 1)) * 100}>
-                <span slot="min">0</span>
-                <span slot="tooltip"
-                  >${Number($preSaleTotalCollateralRaised$?.toFixed(0)).toLocaleString()}</span>
-                <span slot="max">${$preSaleGoal$?.toLocaleString()}</span>
-                <span slot="disabled">{$__$?.presale.notFunding}</span>
-              </ProgressBar>
-            </Card>
+  {/if}
+  {#if !_.isNil($preSaleStatus$) && $preSaleStatus$ !== PreSaleStatus.Pending}
+    <div transition:fade class="max-w-full">
+      <div class="flex flex-col space-y-6">
+        <PageTitle />
+        <div
+          class="flex flex-col items-stretch md:flex-row-reverse justify-center md:space-x-reverse md:space-x-7 space-y-6 md:space-y-0">
+          <SwapCard />
+          <div class="flex flex-col gap-6 justify-between md:w-96">
+            <PreSaleAllocationTable />
+            <div class="flex flex-col justify-center grow">
+              <Card className={{ container: '!overflow-visible' }}>
+                <ProgressBar
+                  isLoading={isLoadingRaised}
+                  disabled={notFunding}
+                  percent={(($preSaleTotalCollateralRaised$ ?? 0) / ($preSaleGoal$ ?? 1)) * 100}>
+                  <span slot="min">0</span>
+                  <span slot="tooltip">
+                    ${Number($preSaleTotalCollateralRaised$?.toFixed(0)).toLocaleString()}
+                  </span>
+                  <span slot="max">${$preSaleGoal$?.toLocaleString()}</span>
+                  <span slot="disabled">{$__$?.presale.notFunding}</span>
+                </ProgressBar>
+              </Card>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="space-y-6">
-        <span>{$__$?.presale.vestings.title}</span>
-        <PresaleVestingTable />
+        <div class="space-y-6">
+          <span>{$__$?.presale.vestings.title}</span>
+          <PresaleVestingTable />
+        </div>
       </div>
     </div>
-  </Fade>
+  {/if}
 </div>
