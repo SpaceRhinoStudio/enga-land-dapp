be19f63842de9690b9e68283f57dd554ce164249
clonomaer <clonomaer@gmail.com>
2022-06-06 15:01:31 +0200

    add fixed ratio contaner

---
+++ src/lib/FixedRatioContainer.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import cn from 'classnames'
+
+  export let className: { [key in 'inner' | 'ratio' | 'container']?: string } = {}
+  export let ratio: number
+</script>
+
+<div class={cn('relative', 'h-[max-content]', className?.container)}>
+  <div class={cn('w-full', className?.ratio)} style="padding-top: {100 / ratio}%;" />
+  <div class={cn('absolute', 'inset-0', className?.inner)}>
+    <slot />
+  </div>
+</div>
