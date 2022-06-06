f36dfe0313e1ab07c350bd5b7ff0fa7ee9930944
clonomaer <clonomaer@gmail.com>
2022-06-06 14:44:05 +0200

    mod add some event prevent on click outside

--- src/lib/actions/click-outside.ts
+++ src/lib/actions/click-outside.ts
@@ -1,7 +1,11 @@
 export function outclick(node: HTMLElement): { destroy: () => void } {
   const listener = (e: MouseEvent) => {
     if (node && !node.contains(e.target as Node) && !e.defaultPrevented) {
+      e.stopPropagation()
+      e.preventDefault()
+      e.stopImmediatePropagation()
       node.dispatchEvent(new CustomEvent('outclick'))
+      return false
     }
   }
 
