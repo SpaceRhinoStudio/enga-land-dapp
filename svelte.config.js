5eb5fdab95dffd0c88e55bda5055018697038a3d
clonomaer <clonomaer@gmail.com>
2022-06-09 19:57:41 +0200

    add cloudflare pages export

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto'
+// import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-cloudflare'
 import preprocess from 'svelte-preprocess'
 import svg from '@poppanator/sveltekit-svg'
 
