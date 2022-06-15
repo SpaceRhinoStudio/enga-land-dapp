8243842851c7f057822efb41e545f3f532ab333c
clonomaer <clonomaer@gmail.com>
2022-06-15 14:29:38 +0200

    move isFirefox to shared

--- src/lib/contexts/is-firefox.ts
+++
@@ -1,7 +0,0 @@
-import { Window$ } from '$lib/shared/observables/window'
-import { map, shareReplay } from 'rxjs'
-
-export const isFirefox$ = Window$.pipe(
-  map(() => !CSS.supports('backdrop-filter', 'blur(1px)')),
-  shareReplay(1),
-)
