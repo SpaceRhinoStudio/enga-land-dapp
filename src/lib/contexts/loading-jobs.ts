83e22ab5145817a308c6461639fe23ccd7878002
clonomaer <clonomaer@gmail.com>
2022-06-17 21:48:02 +0200

    fix navigating/loading

--- src/lib/contexts/loading-jobs.ts
+++
@@ -1,6 +0,0 @@
-import { config } from '$lib/configs'
-import { BehaviorSubject, Observable, timer } from 'rxjs'
-
-export const pageLoadingJobs$ = new BehaviorSubject<Promise<unknown> | Observable<unknown>>(
-  timer(config.Delays.mainLoadingMin),
-)
