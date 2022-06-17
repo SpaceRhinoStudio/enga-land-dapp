3a20fded5aae930e78de7502fdeed5de1d4aa564
clonomaer <clonomaer@gmail.com>
2022-06-17 21:59:54 +0200

    add firefox compatibility

--- src/lib/IndexNavigationItem.svelte
+++ src/lib/IndexNavigationItem.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import { goto } from '$app/navigation'
   import JigglyCard from '$lib/shared/JigglyCard.svelte'
+  import cn from 'classnames'
   import Button from './shared/Button.svelte'
+  import { isFirefox$ } from './shared/contexts/is-firefox'
+  import { canHover$ } from './shared/helpers/media-queries'
   import SvgIcon from './shared/SVGIcon.svelte'
 
   export let title: string | undefined
@@ -14,39 +17,46 @@
   <Button
     {disabled}
     job={() => goto(href)}
-    className={`
-        font-serif
-        tracking-wide
-        border-none
-        backdrop-blur-xl
-        relative
-        !md:p-8
-        !p-7
-        text-text-primary
-        bg-primary-700
-        w-full
-        h-48
-        flex
-        flex-col
-        justify-between
-        rounded-xl
-        bg-opacity-50
-        disabled:bg-opacity-20
-        disabled:hover:brightness-100
-        hover:brightness-125
-        !transition-all
-        text-2xl
-        md:text-2xll
-        space-y-3
-        items-center
-        md:items-start
-        !m-0
-        ${
-          !disabled
-            ? 'hover:shadow-strong !duration-700 scale-100 translate-y-0 hover:scale-105 hover:-translate-y-2 will-change-transform'
-            : ''
-        }
-  `}>
+    className={cn(
+      'font-serif',
+      'tracking-wide',
+      'border-none',
+      $isFirefox$
+        ? cn('disabled:!bg-opacity-100 disabled:!bg-primary-800 disabled:brightness-75')
+        : cn('backdrop-blur-xl', 'bg-opacity-50', 'disabled:bg-opacity-20'),
+      'relative',
+      '!md:p-8',
+      '!p-7',
+      'text-text-primary',
+      'bg-primary-700',
+      'w-full',
+      'h-48',
+      'flex',
+      'flex-col',
+      'justify-between',
+      'rounded-xl',
+      '!transition-all',
+      'text-2xl',
+      'md:text-2xll',
+      'space-y-3',
+      'items-center',
+      'md:items-start',
+      '!m-0',
+      !disabled &&
+        cn(
+          $canHover$ &&
+            cn(
+              'hover:brightness-125',
+              'hover:shadow-strong',
+              'hover:scale-105',
+              'hover:-translate-y-2',
+            ),
+          '!duration-700',
+          'scale-100',
+          'translate-y-0',
+          'will-change-transform',
+        ),
+    )}>
     <div class="md:text-left text-center grow">{title}</div>
     <SvgIcon {Icon} width={'3rem'} height={'3rem'} className="md:self-end" />
   </Button>
