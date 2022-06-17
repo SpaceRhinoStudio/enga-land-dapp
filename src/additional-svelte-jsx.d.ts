67da14e562d73ef80e0580fa55b996c973dc4718
clonomaer <clonomaer@gmail.com>
2022-06-17 21:49:10 +0200

    add events types

--- src/additional-svelte-jsx.d.ts
+++ src/additional-svelte-jsx.d.ts
@@ -1,5 +1,7 @@
 declare namespace svelte.JSX {
   interface HTMLAttributes<T> {
-    onresize?: (e: CustomEvent<import('./lib/types').Bounds>) => void
+    onresize?: (e: CustomEvent<import('./lib/shared/types').Bounds>) => void
+    onintersection?: (e: CustomEvent<boolean>) => void
+    onswipe_dismiss?: (e: CustomEvent<'left' | 'right'>) => void
   }
 }
