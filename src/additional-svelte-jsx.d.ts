598ec950080691aa3f6444a9662f132fd8d08026
clonomaer <clonomaer@gmail.com>
2022-06-21 13:09:03 +0200

    add types fpr slider action

--- src/additional-svelte-jsx.d.ts
+++ src/additional-svelte-jsx.d.ts
@@ -3,5 +3,6 @@ declare namespace svelte.JSX {
     onresize?: (e: CustomEvent<import('./lib/shared/types').Bounds>) => void
     onintersection?: (e: CustomEvent<boolean>) => void
     onswipe_dismiss?: (e: CustomEvent<'left' | 'right'>) => void
+    onslider_change?: (e: CustomEvent<[percent: number, px: number]>) => void
   }
 }
