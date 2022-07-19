47066d0d5f4b31b5544d114892978f4478dd77a9
clonomaer <clonomaer@gmail.com>
2022-07-19 21:54:36 +0200

    add jiggly effect to all showcase cards

--- src/lib/showcase-card/ShowcaseCardRaw.svelte
+++ src/lib/showcase-card/ShowcaseCardRaw.svelte
@@ -2,11 +2,22 @@
   import FlipCard from '../FlipCard.svelte'
   import ShowcaseCardRawFront from './ShowcaseCardRawFront.svelte'
   import cn from 'classnames'
+  import JigglyCard from '$lib/shared/JigglyCard.svelte'
+  import { SpringConfig } from 'wobble'
 
   export let isAutoHeight = false
   export let keepFrontHeight = false
   export let backfaceVisible = false
   export let className: { [key in 'cardHeight']?: string } = {}
+
+  // -- raw front card props
+  export let cornerRadius: string | undefined = undefined
+  export let cutHeight: string | undefined = undefined
+  export let hScale: number | undefined = undefined
+  export let cardHeightClassName: string | undefined = undefined
+
+  let jiggleClassname = `relative z-10 ${cardHeightClassName ?? 'h-full'}`
+  let jiggleConfig: Partial<SpringConfig> = { damping: 5 }
 </script>
 
 {#if $$slots.backface}
@@ -21,18 +32,22 @@
       backfaceWrapper: 'flex',
     }}>
     <slot name="backface" slot="backface" />
-    <ShowcaseCardRawFront>
+    <ShowcaseCardRawFront {cornerRadius} {cutHeight} {hScale} {cardHeightClassName}>
       <slot />
       <slot name="title" slot="title" />
       <slot name="subtitle" slot="subtitle" />
-      <slot name="left" slot="left" />
+      <JigglyCard slot="left" className={jiggleClassname} config={jiggleConfig}>
+        <slot name="left" />
+      </JigglyCard>
     </ShowcaseCardRawFront>
   </FlipCard>
 {:else}
-  <ShowcaseCardRawFront>
+  <ShowcaseCardRawFront {cornerRadius} {cutHeight} {hScale} {cardHeightClassName}>
     <slot />
     <slot name="title" slot="title" />
     <slot name="subtitle" slot="subtitle" />
-    <slot name="left" slot="left" />
+    <JigglyCard slot="left" className={jiggleClassname} config={jiggleConfig}>
+      <slot name="left" />
+    </JigglyCard>
   </ShowcaseCardRawFront>
 {/if}
