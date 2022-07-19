4aa89a565657ce7557babf56c36bd15dee18996a
clonomaer <clonomaer@gmail.com>
2022-07-19 21:45:54 +0200

    fix handle undefined in sanitizeNumbers

--- src/lib/utils/sanitize-numbers.ts
+++ src/lib/utils/sanitize-numbers.ts
@@ -1,5 +1,7 @@
+import _ from 'lodash'
+
 export function sanitizeNumbers(str: string): string {
-  return str
+  return _.isString(str) ? str : ''
 }
 export function onlyNumbers(str: string): string {
   return sanitizeNumbers(str).replace(/[^0-9.]/g, '')
