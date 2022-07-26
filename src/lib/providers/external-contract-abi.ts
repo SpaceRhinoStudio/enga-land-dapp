195fa30630208a2ff565674d184497ff784ba5be
clonomaer <clonomaer@gmail.com>
2022-07-26 23:30:49 +0200

    mod manually added ABIs to not be network dependant

--- src/lib/providers/external-contract-abi.ts
+++ src/lib/providers/external-contract-abi.ts
@@ -1,16 +1,10 @@
-import { map, Observable } from 'rxjs'
+import { Observable, of } from 'rxjs'
 import otherAbis from '../../contracts/other-abis.json'
-import type { Network } from '$lib/configs/web3'
-import { selectedNetwork$ } from '$lib/observables/web3-network'
 
-type ContractAbiDefinitionType = {
-  [key in Network]?: { [contractName: string]: string }
-}
+type ContractAbiDefinitionType = { [contractName: string]: string }
 
 export function externalContractAbi$Factory(
   contractGivenName: string,
 ): Observable<string | undefined> {
-  return selectedNetwork$.pipe(
-    map(x => (otherAbis as ContractAbiDefinitionType)[x]?.[contractGivenName]),
-  )
+  return of((otherAbis as ContractAbiDefinitionType)[contractGivenName])
 }
