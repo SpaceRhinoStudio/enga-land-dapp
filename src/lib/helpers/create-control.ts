204122b412c2d7c15cd2f31b89953e63168cfac2
clonomaer <clonomaer@gmail.com>
2022-08-01 18:41:19 +0200

    doc useCreateControl

--- src/lib/helpers/create-control.ts
+++ src/lib/helpers/create-control.ts
@@ -2,6 +2,9 @@ import { getSubjectValue } from '$lib/utils/get-subject-value'
 import _ from 'lodash'
 import { ReplaySubject } from 'rxjs'
 
+/**
+ * @description this is a helper function to create a control `Subject` with a custom `next` function that preserves all the previous values of the control object so that the control object can be used as a svelte state without problems. there is also an `omit` argument to omit certain properties from being constantly preserved across all emissions of the subject for special cases
+ */
 export function useCreateControl<Control extends _.Dictionary<unknown> = never>(
   args: {
     omit?: (keyof Control)[]
