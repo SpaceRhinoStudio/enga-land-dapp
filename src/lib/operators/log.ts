b35bee3aee97beb53acc13c1ccd73c60ac60653f
clonomaer <clonomaer@gmail.com>
2022-08-11 09:29:38 +0200

    add a lot of functionalities to log operator

--- src/lib/operators/log.ts
+++ src/lib/operators/log.ts
@@ -1,24 +1,74 @@
 import _ from 'lodash'
-import { type MonoTypeOperatorFunction, pipe, tap } from 'rxjs'
+import {
+  type MonoTypeOperatorFunction,
+  pipe,
+  concatMap,
+  map,
+  from,
+  of,
+  reduce,
+  dematerialize,
+  materialize,
+} from 'rxjs'
 import { unLazy } from '$lib/shared/utils/un-lazy'
+import { wrapWith } from '$lib/utils/zone'
+import { isWeb3Error, nameOfWeb3Error } from '$lib/helpers/web3-errors'
+import { isEnumMember } from '$lib/utils/enum'
+import { ActionStatus } from '$lib/types'
+
+const isAction = isEnumMember(ActionStatus)
+
+function transform(x: unknown): unknown[] {
+  if (isWeb3Error(x)) {
+    return ['%c' + nameOfWeb3Error(x), 'color: #f00']
+  }
+  if (isAction(x)) {
+    return ['%c' + x, 'color: #e50']
+  }
+
+  return [x]
+}
 
 export function logOp<T>(
-  ...messages: (string | ((source: T) => unknown))[]
+  ...messages: (string | number | ((source: T) => unknown))[]
 ): MonoTypeOperatorFunction<T> {
+  const zone = Zone.current
   return pipe(
-    tap(async x => {
-      let res: unknown[] = []
-      if (messages.length === 0) {
-        res = [x]
-      } else if (messages.length === 1) {
-        res = [await unLazy(messages[0], x), [x]]
-      } else {
-        for (const curr of messages) {
-          res.push(' ')
-          res.push(_.isString(curr) ? curr.trim() : await unLazy(curr, x))
-        }
-      }
-      console.log(...res)
+    materialize(),
+    concatMap((x, i) => {
+      return from(
+        x.kind !== 'N' ? messages.filter(_.negate(_.isFunction)) : _.castArray(messages),
+      ).pipe(
+        concatMap(curr =>
+          _.isString(curr) ? of(curr.trim()) : from(Promise.resolve(unLazy(curr, x.value!))),
+        ),
+        reduce((acc, curr) => [...acc, ...(acc.length ? [' '] : []), curr], [] as unknown[]),
+        map(res => {
+          wrapWith(
+            zone === Zone.current
+              ? zone
+              : zone.fork({
+                  name: Zone.current.name,
+                  properties: {
+                    fgColor: Zone.current.get('fgColor'),
+                    bgColor: Zone.current.get('bgColor'),
+                  },
+                }),
+            console.debug,
+          )(
+            `%c[${i}]`,
+            'color: #666',
+            ...res,
+            ...(x.kind === 'N'
+              ? transform(x.value)
+              : x.kind === 'C'
+              ? ['OBSERVABLE_COMPLETE']
+              : ['OBSERVABLE_ERROR', x.error]),
+          )
+          return x
+        }),
+      )
     }),
+    dematerialize(),
   )
 }
