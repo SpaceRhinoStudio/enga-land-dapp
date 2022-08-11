29c790fad01772ba46c066dbcaccb667ed1845d6
clonomaer <clonomaer@gmail.com>
2022-08-11 09:07:54 +0200

    nit fix style

--- src/lib/ProgressBar.svelte
+++ src/lib/ProgressBar.svelte
@@ -59,7 +59,7 @@
       </span>
     {/if}
   </div>
-  <LoadingOverlay visible={isLoading} className={{ container: '!top-4' }} />
+  <LoadingOverlay visible={isLoading} />
   {#if disabled}
     <div transition:fade class="absolute inset-0 flex items-center justify-center text-sm">
       <slot name="disabled" />
