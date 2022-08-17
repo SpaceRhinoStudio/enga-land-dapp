426334c288d731eb7df566a510443d62db003c76
clonomaer <clonomaer@gmail.com>
2022-08-17 19:26:05 +0200

    fix zone override by wrapping add more zone utils

--- src/lib/utils/zone.ts
+++ src/lib/utils/zone.ts
@@ -1,12 +1,16 @@
 export type Fn<T extends readonly unknown[], R> = (...args: T) => R
 
+export function copyFn<T extends readonly unknown[], R>(fn: Fn<T, R>): Fn<T, R> {
+  return (...args) => fn(...args)
+}
+
 export function wrap<T extends readonly unknown[], R>(fn: Fn<T, R>): Fn<T, R>
 export function wrap<T extends readonly unknown[], R>(source: string, fn: Fn<T, R>): Fn<T, R>
 export function wrap<T extends readonly unknown[], R>(
   sourceOrFn: Fn<T, R> | string,
   fn?: Fn<T, R>,
 ): Fn<T, R> {
-  return Zone.current.wrap(fn ?? (sourceOrFn as Fn<T, R>), fn ? (sourceOrFn as string) : '')
+  return Zone.current.wrap(copyFn(fn ?? (sourceOrFn as Fn<T, R>)), fn ? (sourceOrFn as string) : '')
 }
 
 export function wrapWith<T extends readonly unknown[], R>(zone: Zone, fn: Fn<T, R>): Fn<T, R>
@@ -20,7 +24,7 @@ export function wrapWith<T extends readonly unknown[], R>(
   sourceOrFn: Fn<T, R> | string,
   fn?: Fn<T, R>,
 ): Fn<T, R> {
-  return zone.wrap(fn ?? (sourceOrFn as Fn<T, R>), fn ? (sourceOrFn as string) : '')
+  return zone.wrap(copyFn(fn ?? (sourceOrFn as Fn<T, R>)), fn ? (sourceOrFn as string) : '')
 }
 
 export function forkWrap<T extends readonly unknown[], R>(name: string, fn: Fn<T, R>): Fn<T, R>
@@ -36,7 +40,19 @@ export function forkWrap<T extends readonly unknown[], R>(
 ): Fn<T, R> {
   return Zone.current
     .fork({ name, properties: fn ? { bgColor: fnOrBg } : {} })
-    .wrap(fn ?? (fnOrBg as Fn<T, R>), '')
+    .wrap(copyFn(fn ?? (fnOrBg as Fn<T, R>)), '')
+}
+
+export function forkFrom(
+  zone: Zone,
+  name: string,
+  colors?: [bgColor: string, fgColor: string],
+): Zone {
+  return zone.fork({ name, properties: { bgColor: colors?.[0], fgColor: colors?.[1] } })
+}
+
+export function fork(name: string, colors?: [bgColor: string, fgColor: string]): Zone {
+  return Zone.current.fork({ name, properties: { bgColor: colors?.[0], fgColor: colors?.[1] } })
 }
 
 export function run<T extends readonly unknown[], R>(fn: Fn<T, R>): R {
