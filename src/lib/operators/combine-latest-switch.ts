e251ffbc7f4c1cea67f43028575718f91bccee2b
clonomaer <clonomaer@gmail.com>
2022-08-17 19:44:41 +0200

    add operator util withLatestFromFlat

--- src/lib/operators/combine-latest-switch.ts
+++ src/lib/operators/combine-latest-switch.ts
@@ -1,4 +1,13 @@
-import { map, type OperatorFunction, switchMap, from, ObservableInput, pipe } from 'rxjs'
+import {
+  map,
+  type OperatorFunction,
+  switchMap,
+  from,
+  ObservableInput,
+  pipe,
+  Observable,
+  withLatestFrom,
+} from 'rxjs'
 import { unLazy } from '$lib/shared/utils/un-lazy'
 import { ShallowCons } from '$lib/types'
 import { castArray } from '$lib/shared/utils/type-safe'
@@ -11,5 +20,14 @@ export function combineLatestSwitchMap<T, R>(
     switchMap(x =>
       from(unLazy(project, x)).pipe(map(res => [...castArray(x), ...castArray(res)] as const)),
     ),
+  )
+}
+
+export function withLatestFromFlat<T, R>(
+  source$: Observable<R>,
+): OperatorFunction<T, ShallowCons<T, R>> {
+  return pipe(
+    withLatestFrom(source$),
+    map(([x, s]) => [...castArray(x), ...castArray(s)] as const),
   )
 }
