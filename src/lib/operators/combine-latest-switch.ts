0f1d0115175422bdfaf65959d83ea8ddb488cbae
clonomaer <clonomaer@gmail.com>
2022-08-17 19:22:11 +0200

    mod bump shared

--- src/lib/operators/combine-latest-switch.ts
+++ src/lib/operators/combine-latest-switch.ts
@@ -1,12 +1,12 @@
 import { map, type OperatorFunction, switchMap, from, ObservableInput, pipe } from 'rxjs'
 import { unLazy } from '$lib/shared/utils/un-lazy'
-import { DeepCons } from '$lib/types'
+import { ShallowCons } from '$lib/types'
 import { castArray } from '$lib/shared/utils/type-safe'
 
 //DEBUG: write tests
 export function combineLatestSwitchMap<T, R>(
   project: (input: T) => ObservableInput<R>,
-): OperatorFunction<T, DeepCons<T, R>> {
+): OperatorFunction<T, ShallowCons<T, R>> {
   return pipe(
     switchMap(x =>
       from(unLazy(project, x)).pipe(map(res => [...castArray(x), ...castArray(res)] as const)),
